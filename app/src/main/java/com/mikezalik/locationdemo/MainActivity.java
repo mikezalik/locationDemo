a46c5eadad23554074d0f2340917c26160d9450c
mikezalik <mike.zalik@gmail.com>
2020-02-13 12:32:07 -0800

    Adds permission for fine location and adds location manager/location listener to onCreate.

--- app/src/main/java/com/mikezalik/locationdemo/MainActivity.java
+++ app/src/main/java/com/mikezalik/locationdemo/MainActivity.java
@@ -2,6 +2,10 @@ package com.mikezalik.locationdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +14,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        LocationListener locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        }
     }
 }
